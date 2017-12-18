baca15286337568a3db3f5f88f993720072ab147
Maria Camila Gómez <camigomez@experimentality.co>
2017-12-17 23:47:03 -0500

    Show symbols in template

---
+++ mySite.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template 
+from bitfinexApi import BitfinexApi
+
+app = Flask(__name__) 
+
+
+btx = BitfinexApi()
+# response = btx.getTicker('btcusd')
+# print(response['mid'])
+
+
+@app.route('/')
+def home():
+    symbols = btx.getSymbols()
+    return render_template("home.html", symbols = symbols)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3001)
